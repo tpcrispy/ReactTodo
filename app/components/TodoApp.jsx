5170143547bfa26c64d5b00a6958cf8c1ef117e1
Thomas Smith <thomas.1993.smith@gmail.com>
2016-06-08 15:06:03 +1000

    added Todo component and setup webpack

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class TodoApp extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+    return (
+      <div>TODO APP</div>
+    );
+  }
+}
+
+export default TodoApp;
